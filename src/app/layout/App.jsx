4b89c4dd6ea93eb5a548144ecee9b49a3d12b6b6
志田守 <>
2018-08-21 11:18:57 +0900

    End Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import Navbar from '../../features/nav/Navbar/Navbar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-vents</h1>
+      <div>
+        <Navbar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
       </div>
     );
   }
 }
 
 export default App;
+
+
